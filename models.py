4f1aa758dd349c022a036ae83440d942b021aa25
Kennedy <krtorres.programador@hotmail.com>
2025-08-30 23:15:33 -0300

    rotas: criar perfil e usuario

--- models.py
+++ models.py
@@ -33,7 +33,7 @@ class Gestor(Base):
     nome = Column("nome", String, nullable=False)
     email = Column("email", String, nullable=False)
     senha = Column("senha", String)
-    empresa_id = Column("empresa_id", Integer, ForeignKey("empresas.id"))
+    empresa_id = Column("empresa_id", Integer, ForeignKey("empresas.id")) # 1 empresa possui vários gestores, mas 1 gestor pertence a apenas 1 empresa
     
     def __init__(self, nome, email, senha, empresa_id):
         self.nome = nome
@@ -50,15 +50,18 @@ class Usuario(Base):
     nome = Column("nome", String)
     email = Column("email", String, nullable=False)
     senha = Column("senha", String)
-    ativo = Column("ativo", Boolean)
-    empresa_id = Column("empresa_id", Integer, ForeignKey("empresas.id"))
+    status = Column("status", String) # ATIVO ou INATIVO
+    perfil_id = Column("perfil_id", Integer, ForeignKey("perfis.id")) # 1 usuário possui 1 perfil, mas 1 perfil pode ser vinculado a vários usuários
+    gestor_id = Column("gestor_id", Integer, ForeignKey("gestores.id")) # 1 gestor pode criar vários usuários, mas 1 usuário é criado por apenas 1 gestor
     
-    def __init__(self, nome, email, senha, ativo=True, empresa_id=None):
+    def __init__(self, nome, email, senha, perfil_id, gestor_id, status="ATIVO"):
         self.nome = nome
         self.email = email
         self.senha = senha
-        self.ativo = ativo
-        self.empresa_id = empresa_id
+        self.perfil_id = perfil_id
+        self.gestor_id = gestor_id
+        self.status = status
+        
         
 class Perfil(Base):
     __tablename__ = "perfis"
@@ -66,10 +69,9 @@ class Perfil(Base):
     id = Column("id", Integer, primary_key=True, autoincrement=True)
     nome = Column("nome", String, nullable=False)
     permissoes = Column("permissoes", String) # lista de permissões vinculadas ao perfil
-    usuario_id = Column("usuario_id", Integer, ForeignKey("usuarios.id"))
     
-    def __init__(self, nome, permissoes, usuario_id):
+    def __init__(self, nome, permissoes):
         self.nome = nome
         self.permissoes = permissoes
-        self.usuario_id = usuario_id
+        
 # executa a criação dos metadados do banco (criação efetiva do banco)
--- order_routes.py
+++ order_routes.py
@@ -1,7 +1,35 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from models import Usuario, Perfil
+from dependencies import pegar_sessao
+from schemas import UsuarioSchema, PerfilSchema
+from sqlalchemy.orm import Session
 
 order_router = APIRouter(prefix="/usuarios", tags=["usuarios"])
 
 @order_router.get("/")
 async def usuarios():
+    """_summary_: Essa é a rota padrão de usuários do sistema. Todas as rotas de usuários precisam de autenticação.        
+    """
     return {"mensagem": "Você acessou a rota de usuários"}
+
+@order_router.post("/usuario")
+async def criar_usuario(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao)):
+    """_summary_: Rota para criação de um novo usuário. Apenas gestores autenticados podem criar usuários.
+    
+    - **usuario_schema**: Dados do usuário a ser criado, conforme o esquema definido em UsuarioSchema.
+    """
+    novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, usuario_schema.senha,  usuario_schema.perfil_id, usuario_schema.gestor_id)
+    session.add(novo_usuario)
+    session.commit()
+    return {"mensagem": f"Usuário criado com sucesso. Usuario: {usuario_schema.email}"}
+
+@order_router.post("/perfil")
+async def criar_perfil(perfil_schema: PerfilSchema, session: Session = Depends(pegar_sessao)):
+    """_summary_: Rota para criação de um novo perfil. Apenas gestores autenticados podem criar perfis.
+    
+    - **Perfil**: Cada perfil define um conjunto de permissões que podem ser atribuídas ao usuário. O usuário pode ter apenas um perfil, mas um perfil pode ser atribuído a vários usuários.
+    """
+    novo_perfil = Perfil(perfil_schema.nome, ",".join(perfil_schema.permissoes)) # Converte a lista de permissões em uma string separada por vírgulas
+    session.add(novo_perfil)
+    session.commit()
+    return {"mensagem": f"Perfil criado com sucesso. Perfil: {perfil_schema.nome}"}
