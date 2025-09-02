3e51e6934193ac43d71ab60e53f868a1ef3aaa6b
Matheus Vinycius Vieira Batista <vinyciusmatheus58@gmail.com>
2025-09-02 14:19:21 -0300

    X:base

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Criador_de_perfis from "./pages/Criador_de_perfis";
+
+function App(){
+  return (
+    <>
+      <Criador_de_perfis/>
+    </>
+  )
+}
+
+export default App
