b4bbd20f90a312ac58992cdd86336d1583c29878
Kennedy <krtorres.programador@hotmail.com>
2025-10-02 04:45:50 -0300

    feat(repository): Cria BaseRepository genérico para operações CRUD
    implementação genérica do padrão Repositório para centralizar as operações comuns de banco de dados (CRUD)
    
    - Reutilização de Código: Utiliza `Generic` e `TypeVar` do Python para criar uma classe base que pode ser estendida por repositórios específicos (como `UserRepository`, `PerfilRepository`, etc.).
    
    - Abstração do CRUD: Fornece implementações padrão para os métodos `get`, `create`, `update` e `remove`, evitando a repetição de código boilerplate em cada repositório da aplicação.
    
    - Princípio DRY: Segue o princípio "Don't Repeat Yourself" (Não se Repita), o que torna a camada de acesso a dados mais limpa, consistente e fácil de manter.

---
+++ app/repositories/base_repository.py
@@ -0,0 +1,34 @@
+from typing import Any, Generic, Type, TypeVar
+from sqlalchemy.orm import Session
+from app.core.database import Base
+
+ModelType = TypeVar("ModelType", bound=Base)
+
+#CRUD genérico
+class BaseRepository(Generic[ModelType]):
+    def __init__(self, model: Type[ModelType]):
+        self.model = model
+
+    def get(self, db: Session, id: Any) -> ModelType | None:
+        return db.query(self.model).filter(self.model.id == id).first()
+
+    def create(self, db: Session, *, obj_in) -> ModelType:
+        db_obj = self.model(**obj_in.dict())
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def update(self, db: Session, *, db_obj: ModelType, obj_in) -> ModelType:
+        # Implementação de update...
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def remove(self, db: Session, *, id: int) -> ModelType:
+        # Implementação de remoção...
+        obj = db.query(self.model).get(id)
+        db.delete(obj)
+        db.commit()
+        return obj
